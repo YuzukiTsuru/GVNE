d6306f50622c929f8b55937f538cf4624e49c3e7
740291272 <740291272@qq.com>
2019-10-07 01:21:23 +0800

    add CMD log

--- Src/Main.cpp
+++ Src/Main.cpp
@@ -1,8 +1,14 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
+
 #include "Main.h"
+#include "ApplicationInfo.h"
 
 int main(int argc, char *argv[]) {
+
+    std::cout << "G V N E \n"
+              << ProjectInfo::VERSION
+              << std::endl;
 
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 
@@ -10,8 +16,10 @@ int main(int argc, char *argv[]) {
 
     QQmlApplicationEngine engine;
     engine.load(QUrl(QStringLiteral("qrc:QML/main.qml")));
+
     if (engine.rootObjects().isEmpty()) {
         return -1;
     }
+
     return QGuiApplication::exec();
 }
